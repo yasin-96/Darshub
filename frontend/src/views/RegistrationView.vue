304e8287d8c9b36a6942c518066582277576a895
Alexander Jajzler <dev@jajzler.de>
2021-10-11 22:07:42 +0200

    frontend: format view (#3)

--- frontend/src/views/RegistrationView.vue
+++ frontend/src/views/RegistrationView.vue
@@ -1,50 +1,116 @@
 <template>
-  <v-container>
+  <v-container fill-height align="center" justify="center">
     <v-row>
-      <v-col>
-        <v-card outlined shaped>
-          <v-card-title class="headline"> Darshub-Konto erstellen</v-card-title>
+      <v-col class="mx-auto" cols="12" sm="12" md="12" lg="8" xl="6">
+        <v-card outlined shaped elevation="2">
+          <v-card-title class="headline">
+            <v-container>
+              <v-row>
+                <v-col>Darshub-Konto erstellen</v-col>
+              </v-row>
+            </v-container>
+          </v-card-title>
           <v-card-text>
             <v-form ref="reg_registryForm" v-model="valid" lazy-validation>
               <v-row dense>
                 <v-col>
-                  <v-text-field v-model="name" label="Name" required prepend-icon="mdi-account-circle"></v-text-field>
+                  <v-text-field
+                    v-model="name"
+                    label="Name"
+                    required
+                    prepend-icon="mdi-account-circle"
+                  ></v-text-field>
                 </v-col>
-                <v-col cols="3">
-                  <v-menu ref="ref_birthday" v-model="birthdayModel" :close-on-content-click="false" :return-value.sync="date" transition="scale-transition" offset-y min-width="auto">
+                <v-col cols="12" sm="12" md="3" lg="3" xl="3">
+                  <v-menu
+                    ref="ref_birthday"
+                    v-model="birthdayModel"
+                    :close-on-content-click="false"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="auto"
+                  >
                     <template v-slot:activator="{ on, attrs }">
-                      <v-text-field v-model="date" label="Birthday" prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on"></v-text-field>
+                      <v-text-field
+                        v-model="birthday"
+                        label="Birthday"
+                        prepend-icon="mdi-calendar"
+                        readonly
+                        v-bind="attrs"
+                        v-on="on"
+                      ></v-text-field>
                     </template>
-                    <v-date-picker v-model="birthday" no-title scrollable>
-                      <v-spacer></v-spacer>
-                      <v-btn text color="primary" @click="disableMenu()"> Cancel </v-btn>
-                      <v-btn text color="primary" @click="saveBirthday()"> OK </v-btn>
-                    </v-date-picker>
+                    <v-date-picker
+                      v-model="birthday"
+                      :active-picker.sync="birthdayPicker"
+                      :max="
+                        new Date(
+                          Date.now() - new Date().getTimezoneOffset() * 60000
+                        )
+                          .toISOString()
+                          .substr(0, 10)
+                      "
+                      min="1950-01-01"
+                      @change="saveBirthday"
+                    ></v-date-picker>
                   </v-menu>
                 </v-col>
-                <v-col cols="2">
-                  <v-combobox :items="genders" prepend-icon="mdi-gender-male-female"> </v-combobox>
+                <v-col cols="12" sm="12" md="2" lg="2" xl="2">
+                  <v-combobox
+                    v-model="bio"
+                    :items="genders"
+                    prepend-icon="mdi-gender-male-female"
+                  >
+                  </v-combobox>
                 </v-col>
               </v-row>
               <v-row dense>
                 <v-col>
-                  <v-text-field v-model="email" label="E-mail" required prepend-icon="mdi-email"> </v-text-field>
+                  <v-text-field
+                    v-model="email"
+                    label="E-mail"
+                    required
+                    prepend-icon="mdi-email"
+                  >
+                  </v-text-field>
                 </v-col>
                 <v-col cols="5">
-                  <v-text-field v-model="telNr" label="E-mail" required prepend-icon="mdi-cellphone-basic"> </v-text-field>
+                  <v-text-field
+                    v-model="telNr"
+                    label="E-mail"
+                    required
+                    prepend-icon="mdi-cellphone-basic"
+                  >
+                  </v-text-field>
                 </v-col>
               </v-row>
               <v-row dense>
                 <v-col>
-                  <v-file-input show-size label="Avatar" @change="selectFile" prepend-icon="mdi-badge-account" :hint="`Max file size is 2MB`"></v-file-input>
+                  <v-file-input
+                    show-size
+                    label="Avatar"
+                    @change="selectFile"
+                    prepend-icon="mdi-badge-account"
+                    :hint="`Max file size is 2MB`"
+                  ></v-file-input>
                 </v-col>
               </v-row>
             </v-form>
           </v-card-text>
           <v-card-actions>
-            <v-btn text :disabled="!valid" color="success" class="mr-4" @click="validate"> Erstellen </v-btn>
+            <v-btn
+              text
+              :disabled="!valid"
+              color="success"
+              class="mr-4"
+              @click="validate"
+            >
+              Erstellen
+            </v-btn>
 
-            <v-btn text color="error" class="align-right" @click="reset"> Abbrechen </v-btn>
+            <v-btn text color="error" class="align-right" @click="reset">
+              Abbrechen
+            </v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -59,7 +125,8 @@ export default {
   data: () => ({
     valid: null,
     date: null,
-    birthdayModel: null,
+    birthdayModel: false,
+    birthdayPicker: null,
   }),
   methods: {
     validate() {},
@@ -69,8 +136,9 @@ export default {
     disableMenu() {
       this.birthdayModel = false;
     },
-    saveBirthday() {
-      this.$refs.menu.save(this.date);
+    saveBirthday(newDate) {
+      console.log(newDate);
+      this.$refs.ref_birthday.save(newDate);
     },
     selectFile(newAvatarFile) {
       this.avatar = newAvatarFile;
@@ -193,6 +261,11 @@ export default {
     //   }
     //   return true;
     // },
+  },
+  watch: {
+    birthdayModel(event) {
+      event && setTimeout(() => (this.birthdayPicker = "YEAR"));
+    },
   },
 };
 </script>
