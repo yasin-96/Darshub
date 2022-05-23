358a4c9b706a5aa05989872e573203c8788446fb
Alexander Jajzler <alex@darshub.onmicrosoft.com>
2022-05-23 20:15:51 +0200

    frontend: Disable the vue webconfig options

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,4 @@
-const { GitRevisionPlugin } = require('git-revision-webpack-plugin');
+// const { GitRevisionPlugin } = require('git-revision-webpack-plugin');
 
 module.exports = {
   transpileDependencies: ["vuetify"],
@@ -7,23 +7,23 @@ module.exports = {
     https: false,
     port: 88
   },
-  chainWebpack: (config) => {
-    config
-    .plugin('define')
-    .tap((args) => {
-      const gitRevisionPlugin = new GitRevisionPlugin({
-        commithashCommand: 'rev-list --max-count=1 --no-merges HEAD',
-        versionCommand: 'describe --always --tags',
-      });
+  // chainWebpack: (config) => {
+  //   config
+  //   .plugin('define')
+  //   .tap((args) => {
+  //     const gitRevisionPlugin = new GitRevisionPlugin({
+  //       commithashCommand: 'rev-list --max-count=1 --no-merges HEAD',
+  //       versionCommand: 'describe --always --tags',
+  //     });
 
-      args[0]['process.env']['VUE_APP_GIT_VERSION'] = JSON.stringify(gitRevisionPlugin.version());
-      args[0]['process.env']['VUE_APP_GIT_HASH'] = JSON.stringify(gitRevisionPlugin.commithash());
-      args[0]['process.env']['VUE_APP_GIT_LAST_COMMIT_DATE'] = JSON.stringify(gitRevisionPlugin.lastcommitdatetime());
-      return args;
-    });
+  //     args[0]['process.env']['VUE_APP_GIT_VERSION'] = JSON.stringify(gitRevisionPlugin.version());
+  //     args[0]['process.env']['VUE_APP_GIT_HASH'] = JSON.stringify(gitRevisionPlugin.commithash());
+  //     args[0]['process.env']['VUE_APP_GIT_LAST_COMMIT_DATE'] = JSON.stringify(gitRevisionPlugin.lastcommitdatetime());
+  //     return args;
+  //   });
 
-    config.module.rule('pdf')
-      .test(/\.pdf$/)
-      .use('file-loader').loader('file-loader');
-  },
+  //   config.module.rule('pdf')
+  //     .test(/\.pdf$/)
+  //     .use('file-loader').loader('file-loader');
+  // },
 };
