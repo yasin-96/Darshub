4a27553c66d575aa94ac1bbfbb96629a57f236a5
Alexander Jajzler <alex@darshub.onmicrosoft.com>
2022-12-13 21:17:39 +0100

    Added missing return statement for searchCourses

--- backend/src/CourseSearchService/data/courseSearch.go
+++ backend/src/CourseSearchService/data/courseSearch.go
@@ -2,7 +2,6 @@ package data
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	courseService "dev.azure.com/learn-website-orga/_git/learn-website/src/CourseService/data"
@@ -18,7 +17,8 @@ func SearchCourse(searchTerm string) []courseService.Course {
 	defer client.Disconnect(ctx)
 
 	col := client.Database("darshub").Collection("course")
-	searchStage := bson.A{bson.D{{"$search", bson.D{{"index", "name"}, {"text", bson.D{{"path", "name"}, {"query", searchTerm}}}}}}}
+	searchStage := bson.A{bson.D{{"$search", bson.D{{"index", "adn_autocomplete"}, {"autocomplete", bson.D{{"path", "name"}, {"query", searchTerm}}}}}}}
+	//searchStage := bson.A{bson.D{{"$search", bson.D{{"index", "adn_autocomplete"}, {"text", bson.D{{"path", "name"}, {"query", searchTerm}}}}}}}
 	//projectStage := bson.D{{Key: "$project", Value: bson.D{{Key: "name", Value: 1}, {Key: "_id", Value: 0}}}}
 	//opts := options.Aggregate().SetMaxTime(5 * time.Second)
 	cursor, err := col.Aggregate(ctx, searchStage)
@@ -27,11 +27,7 @@ func SearchCourse(searchTerm string) []courseService.Course {
 		log.Printf("test %s", err)
 	}
 	if err = cursor.All(context.TODO(), &courses); err != nil {
-	}
-	println(len(courses))
-	//println("testo", courses)
-	for _, result := range courses {
-		fmt.Println("out", result.Name)
+		panic(err)
 	}
 	return courses
 }
--- backend/src/CourseSearchService/handlers/courseSearch.go
+++ backend/src/CourseSearchService/handlers/courseSearch.go
@@ -2,13 +2,27 @@ package handlers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"reflect"
 
 	courseSearchService "dev.azure.com/learn-website-orga/_git/learn-website/src/CourseSearchService/data"
+	"dev.azure.com/learn-website-orga/_git/learn-website/src/util"
 )
 
 func SearchCourse(rw http.ResponseWriter, r *http.Request) {
 	searchTerm := r.URL.Query().Get("course")
 	fmt.Println("Search term:", searchTerm)
-	courseSearchService.SearchCourse(searchTerm)
+	respData := courseSearchService.SearchCourse(searchTerm)
+
+	if reflect.ValueOf(respData).IsZero() {
+		rw.WriteHeader(http.StatusNotFound)
+		rw.Write([]byte("No Data found"))
+		return
+	}
+	parseErr := util.ToJSON(respData, rw)
+	if parseErr != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		log.Print(parseErr)
+	}
 }
